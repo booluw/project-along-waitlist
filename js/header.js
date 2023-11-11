6f1d14b6cd24ae4b8b4cb4007135f033907c1487
= <=>
2023-11-11 15:45:06 +0100

    :tada: 'How Along Works' section

--- js/header.js
+++ js/header.js
@@ -1,11 +1,11 @@
 const pageHeader = document.querySelector('header')
 
-export const fixHeader = function {
+export const fixHeader = function () {
   window.onscroll = () => {
-    if (document.documentElement.scrollTop >= 10) {
-      pageHeader.classList.add("header--bg-white");
+    if (document.documentElement.scrollTop >= 20) {
+      pageHeader.classList.add("header--scroll");
     } else {
-      pageHeader.classList.remove("header--bg-white");
+      pageHeader.classList.remove("header--scroll");
     }
   };
 }
