a4d075c4197ab5c990ca9e903a1ecd89269a5aeb
= <=>
2023-11-12 04:40:01 +0100

    :tada: Board and pill animations

--- js/how-it-works.js
+++ js/how-it-works.js
@@ -1,4 +1,5 @@
-const pills = document.querySelectorAll(".pill");
+import gsap from "gsap";
+
 const activePill = document.querySelector(".pill--active");
 
 export const showActivePill = function (element = activePill) {
@@ -9,9 +10,37 @@ export const showActivePill = function (element = activePill) {
   );
 
   for (let index = 0; index < parentBoard.length; index++) {
-    parentBoard[index].classList.remove("board--active");
+    gsap.fromTo(
+      parentBoard[index],
+      {
+        display: "none",
+        opacity: 0.7,
+        duration: 3,
+        translateY: "-10px",
+        ease: "sine.out",
+      },
+      {
+        opacity: 1,
+        translateY: "0px",
+        duration: .3,
+      }
+    );
   }
-  activeBoard.classList.add("board--active")
+  gsap.fromTo(
+    activeBoard,
+    {
+      duration: 1,
+      opacity: 0.7,
+      translateY: "-10px",
+      ease: "sine.in",
+    },
+    {
+      opacity: 1,
+      display: "flex",
+      translateY: "0px",
+      duration: .3,
+    }
+  );
 };
 
 export const changePill = function (element) {
@@ -21,4 +50,10 @@ export const changePill = function (element) {
   }
   element.classList.add("pill--active");
   showActivePill(element);
+}
+
+export const scrollToView = function () {
+  document.querySelector('[name="how-it-works"]').scrollIntoView({
+    behavior: 'smooth'
+  })
 }
